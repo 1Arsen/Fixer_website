4532481fa8d5df75596a32b79286769af058b3d3
AndrewMag13 <nnaaiillburdasov@gmail.com>
2024-04-26 02:44:08 +0400

    fixed map and contacts

--- src/app/views/maps/maps.tsx
+++ src/app/views/maps/maps.tsx
@@ -4,15 +4,12 @@ import { useMemo, useState } from 'react'
 
 const MapsPage = () => {
   const [zoom, setZoom] = useState(16)
-  const mapState = useMemo(
-    () => ({ center: [46.3745228, 48.0504233], zoom }),
-    [zoom]
-  )
+
   return (
     <>
       <div className={s.map__container}>
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d5508.7343778821!2d37.53960494565551!3d55.749775310740056!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sen!2sru!4v1714083107753!5m2!1sen!2sru"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3591.7632840922624!2d37.53400739895949!3d55.7502086920191!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54bdc1a3c70b7%3A0x6a7f9f968e06e667!2sBashnya%20Federatsiya!5e1!3m2!1sen!2sru!4v1714084987869!5m2!1sen!2sru"
           width="330"
           height="330"
           allowFullScreen
